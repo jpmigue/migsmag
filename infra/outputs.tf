0c7ae945b961542b857a75366c05c472cbf6a070
James Migue <18027734+jpmigue@users.noreply.github.com>
2026-01-31 10:22:09 +0000

    Corrected CDN resource local name

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,9 +3,9 @@ output "bucket_name" {
 }
 
 output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+  value = aws_cloudfront_distribution.site.domain_name
 }
 
 output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.cdn.id
+  value = aws_cloudfront_distribution.site.id
 }
